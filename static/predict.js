291541ca75ae0972d37d1fdaffd96e92afba3bc3
Aleksandr887 <a.klyushnichenko@csc.sibsutis.ru>
2022-04-06 23:37:54 +0700

    last at this day

--- static/predict.js
+++ static/predict.js
@@ -12,7 +12,7 @@
 let model;
 (async function() {
     model = await tf.loadLayersModel('mobile_net/model.json');
-    alert("donemod");
+    console.log(model.summary())
 })();
 
 $("#predict-button").click(async function () {
