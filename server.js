36fdb51fd5847fae066d5c8615b1510420b3bcd8
Aleksandr887 <a.klyushnichenko@csc.sibsutis.ru>
2022-04-07 18:33:08 +0700

    hard link

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-let express = require("express");
-let path = require('path');
+import * as tf from '@tensorflow/tfjs';
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+//let tf  = require("@tensorflow/tfjs");
+//let express = require("express");
+//let path = require('path')
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 let app = express();
 
 const PORT = process.env.PORT || 80
@@ -12,6 +21,22 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + "/static"));
 
 app.listen(PORT, function() {
-    console.log("Serving static on 81");
-    console.log(__dirname + "/static");
+    console.log(PORT);
+    console.log('C:/Users/lipuc/Desktop/test_prog/TensorFlowJS/static/mobile_net/model.json');
 });
+
+class AI {
+
+    compile() {
+        const model = tf.loadLayersModel('https://aqueous-caverns-68838.herokuapp.com/static/mobile_net/model.json');
+        console.log("model succses");
+    }
+
+    run() {
+
+    }
+
+}
+
+const ai = new AI();
+ai.compile();
